dac347c71aa758ee7a5e1058e4693625d977f563
PJ <pjbarry@gmail.com>
2013-09-18 20:27:42 +0100

    Empty Fourth Solution and tests

---
+++ KodeKata2/BinarySearchFour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodeKata
+{
+    class BinarySearchFour : BinarySearch
+    {
+        public int chop(int searchval, int[] searchdomain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

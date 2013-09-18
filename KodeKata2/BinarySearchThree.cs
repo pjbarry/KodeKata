bcb109b414cb2afd188a51fdc7938544630e8fb9
PJ <pjbarry@gmail.com>
2013-09-18 11:41:51 +0100

    Solution using Array.BinarySearch Method

--- KodeKata2/BinarySearchThree.cs
+++ KodeKata2/BinarySearchThree.cs
@@ -4,9 +4,15 @@ namespace KodeKata
 {
     public class BinarySearchThree : BinarySearch
     {
+        //Solution using the built-in BinarySearch method for an array
         public int chop(int searchval, int[] searchdomain)
         {
-            throw new NotImplementedException();
+            var pos = Array.BinarySearch(searchdomain, searchval);
+            if (pos < 0)
+            {
+                pos = -1;
+            }
+            return pos;
         }
     }
 }
